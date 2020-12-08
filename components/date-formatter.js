fba72f16297284952cfa001304703a94d047f365
chengze <paladinze@hotmail.com>
2020-12-09 02:25:33 +0800

    refactor(home): change date format

--- components/date-formatter.js
+++ components/date-formatter.js
@@ -1,6 +1,9 @@
 import { parseISO, format } from 'date-fns'
+import { zhCN } from 'date-fns/locale'
 
 export default function DateFormatter({ dateString }) {
   const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, 'LLLL	d, yyyy')}</time>
+  return <time dateTime={dateString}>{format(date, 'yyyy年MM月dd日', {
+    locale: zhCN
+  })}</time>
 }
--- pages/index.js
+++ pages/index.js
@@ -16,23 +16,6 @@ export default function Index({ allPosts }) {
         </Head>
         <Container>
           <Intro />
-          <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
-            <div className="flex-shrink-0">
-              <div className="h-12 w-12 relative">
-                <Image
-                    className="h-12 w-12"
-                    src="/assets/blog/thief/thief.jpg"
-                    alt="Picture of the author"
-                    layout="fill"
-                    objectFit="contain"
-                />
-              </div>
-            </div>
-            <div>
-              <div className="text-xl font-medium text-black">ChitChat</div>
-              <p className="text-gray-500">You have a new message!</p>
-            </div>
-          </div>
           {morePosts.length > 0 && <MoreStories posts={morePosts} />}
         </Container>
       </Layout>
