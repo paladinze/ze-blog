28f859b83daca9bb7cd2320ce9af44b2e59b42f4
chengze <ze.cheng@thoughtworks.com>
2020-12-08 16:15:02 +0800

    base commit

--- pages/index.js
+++ pages/index.js
@@ -8,26 +8,15 @@ import Head from 'next/head'
 import { CMS_NAME } from '../lib/constants'
 
 export default function Index({ allPosts }) {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
+  const morePosts = allPosts.slice(0)
   return (
     <>
       <Layout>
         <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
+          <title>Paladinze</title>
         </Head>
         <Container>
           <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
           {morePosts.length > 0 && <MoreStories posts={morePosts} />}
         </Container>
       </Layout>
@@ -40,7 +29,6 @@ export async function getStaticProps() {
     'title',
     'date',
     'slug',
-    'author',
     'coverImage',
     'excerpt',
   ])
