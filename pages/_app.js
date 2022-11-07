70957ebed9d95f0288e2a1f3ba5b632171ba6330
paladinze <paladinze@hotmail.com>
2022-11-07 11:23:09 -0500

    feat(monitor): enable user analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import '../styles/index.css'
 import "prismjs/themes/prism-tomorrow.css";
 import useTheme from "../components/hooks/useTheme";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function MyApp({Component, pageProps}) {
 
   useTheme();
 
-  return <Component {...pageProps} />
+  return <>
+    <Component {...pageProps} />
+    <Analytics />
+  </>
 }
